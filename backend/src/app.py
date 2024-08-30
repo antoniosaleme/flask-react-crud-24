d5ba55666e38e1599dd2b946ee1acf9fd9c69b8c
thusspokedata <thusspokedata@protonmail.com>
2024-08-30 21:52:38 +0200

    getUsers, getUser, Post

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from flask_pymongo import PyMongo
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 
 # Crear una instancia de la clase Flask
@@ -11,20 +11,35 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/flask-react-crud"
 # Inicializar PyMongo con la aplicación Flask
 mongo = PyMongo(app)
 
-db = mongo.db.users
+users = mongo.db.users
 
 @app.route('/users', methods=['POST'])
 def createUser():
-    print(request.json)
-    return "<h1>received</h1>"
+    id = users.insert_one({
+        "name": request.json["name"],
+        "email": request.json["email"],
+        "password": request.json["password"]
+    }).inserted_id 
+    return jsonify(str(ObjectId(id)))
+   
 
 @app.route('/users', methods=['GET'])
 def getUsers():
-    return "<h1>received</h1>"
+    usersList = []
+    for doc in users.find():
+        doc['_id'] = str(doc['_id'])
+        usersList.append(doc)
+    return jsonify(usersList)
+    
 
 @app.route('/users/<id>', methods=['GET'])
-def getUser():
-    return "<h1>received</h1>"
+def getUser(id):
+    user = users.find_one({"_id": ObjectId(id)})  # Convierte el id a ObjectId para la búsqueda
+    if user:
+        user['_id'] = str(user['_id'])  # Convierte el ObjectId a una cadena, porque objetId no es serializable
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 @app.route('/users/<id>', methods=['DELETE'])
 def deleteUser():
